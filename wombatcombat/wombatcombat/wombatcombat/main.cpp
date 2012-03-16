61aee26aeb11349d5cc08ce841812bc3e2c787fd
Simon Jentsch <sjentsch@me.com>
2012-03-16 19:31:29 +0100

    Auto window size + fullscreen

--- wombatcombat/wombatcombat/wombatcombat/main.cpp
+++ wombatcombat/wombatcombat/wombatcombat/main.cpp
@@ -2,10 +2,20 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Network.hpp>
 
+#include <string>
+
+using namespace std;
+
 int main (int argc, const char * argv[])
 {
+  
+  string gameName = "Wombat Combat";
+  
+  bool fullscreen = false;
+  
   // Create the main window
-  sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+  sf::VideoMode DesktopMode = sf::VideoMode::getDesktopMode(); 
+  sf::RenderWindow window(sf::VideoMode(DesktopMode.width/2, DesktopMode.height/2), gameName);
 
   // Start the game loop
   while (window.isOpen())
@@ -21,6 +31,17 @@ int main (int argc, const char * argv[])
   		// Escape pressed : exit
   		if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
   			window.close();
+      
+      //Fullscreen toggle 
+      if ((event.type == sf::Event::KeyPressed) && (event.key.code == sf::Keyboard::F)) { 
+        fullscreen = !fullscreen; 
+        if (fullscreen)
+          window.create(sf::VideoMode(DesktopMode.width, DesktopMode.height,DesktopMode.bitsPerPixel), gameName, (fullscreen ? sf::Style::Fullscreen : sf::Style::Resize|sf::Style::Close)); 
+        else
+          window.create(sf::VideoMode(DesktopMode.width/2, DesktopMode.height/2,DesktopMode.bitsPerPixel), gameName); 
+        
+      } 
+      
   	}
 
   	// Clear screen
