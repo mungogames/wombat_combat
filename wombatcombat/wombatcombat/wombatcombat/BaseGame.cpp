f26633e34e9ad97b0899ee38c75e7bf7c0ada7ef
Simon Jentsch <sjentsch@me.com>
2012-03-17 15:00:11 +0100

    merge von interaction

--- wombatcombat/wombatcombat/wombatcombat/BaseGame.cpp
+++ wombatcombat/wombatcombat/wombatcombat/BaseGame.cpp
@@ -17,17 +17,14 @@ BaseGame::BaseGame(GameContainer* gc)
 	this->player = new Player(gc, 60, 10, 0.4f, 1.8f);
 	this->camera = new Camera(gc, this->player, sf::Vector2f(4, -3), sf::Vector2f(30, 30*windowRatio));
 
-	this->terrain = new Terrain(gc, 5, 0, 0);
-  	  this->terrain->addPoint(0, 40, 50);
-  	  this->terrain->addPoint(1, 50, 0);
-  	  this->terrain->addPoint(2, 90, 5);
-  	  this->terrain->addPoint(3, 110, 10);
-  	  this->terrain->addPoint(4, 120, 50);
-
-  	this->rock1 = new Rock(gc, 3, 0, 200);
-  	  this->rock1->addPoint(0,	-5,	0);
-  	  this->rock1->addPoint(1,	5,	0);
-  	  this->rock1->addPoint(2,	0,	-10);
+	this->terrain = new Terrain(gc, 7, 0, 0);
+  this->terrain->addPoint(0, 40, 50);
+  this->terrain->addPoint(1, 50, 0);
+  this->terrain->addPoint(2, 90, 5);
+  this->terrain->addPoint(3, 110, 10);
+  this->terrain->addPoint(4, 120, 50);
+  this->terrain->addPoint(5, 200, 45);
+  this->terrain->addPoint(6, 250, 55);
 
   this->terrain->generate();
 }
@@ -52,7 +49,6 @@ void BaseGame::render()
   */
   this->player->render(this->gc);
   this->terrain->render(this->gc);
-  this->rock1->render(this->gc);
   
   
   
