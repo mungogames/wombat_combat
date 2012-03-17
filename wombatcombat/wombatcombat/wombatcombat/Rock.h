7f6c7a428999dfa17327a99888fc645c820199b0
Mobile <Mobile@mySN-XMG-A501>
2012-03-17 12:26:47 +0100

    rock hinzugefügt

---
+++ wombatcombat/wombatcombat/wombatcombat/Rock.h
@@ -0,0 +1,37 @@
+/* Rock.h
+ *
+ *  Created on: 17.03.2012
+ *      Author: sk
+ */
+
+#ifndef wombatcombat_Rock_h
+#define wombatcombat_Rock_h
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <Box2D/Box2D.h>
+
+#include "GameContainer.h"
+
+class Rock
+{
+	private:
+	int pointCount;
+
+	b2Vec2* chainVertices;
+	sf::VertexArray renderVertices;
+
+	b2BodyDef* bodyDef;
+	b2Body* body;
+
+	b2ChainShape* chain;
+
+	public:
+	Rock (GameContainer* gc, int pointCount, float posX, float posY);
+	void addPoint(int Index, float x, float y);
+	void generate();
+
+	void render(GameContainer* gc);
+};
+
+#endif /* wombatcombat_Rock_h */
